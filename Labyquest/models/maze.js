6e33e698dda0a89d1947c34665f9e81b26ac657a
Aironsoft <aironsoft@yandex.ru>
2016-02-13 00:47:56 +0500

    Добавлены заготовки для статистики

---
+++ Labyquest/models/maze.js
@@ -0,0 +1,20 @@
+﻿var util = require('util'), EventEmitter = require('events').EventEmitter;
+
+var Maze = module.exports = function () {
+
+    EventEmitter.call(this);// Инициализируем события
+    
+    this.games = [];// Массив [id игры = объект игры]
+    
+    this.users = [];// Массив [подключённых пользователей = id игры]
+    
+    this.free = [];// Массив пользователей, ожидающих оппонентов для начало игры
+    
+    // Размеры поля
+    this.x = 30;
+    this.y = 30;
+
+    // Время на игру (минуты)
+    this.stepsToWin = 4;
+}
+util.inherits(Maze, EventEmitter);
